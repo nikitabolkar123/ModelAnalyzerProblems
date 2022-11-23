19995a8f20c263e70ffb13dca44708c8b08ce7e0
nikitabolkar123 <bolkarnikita2001@gmail.com>
2022-11-23 09:42:12 -0800

    Added Solution

---
+++ MoodAnalyzerProblem/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MoodAnalyzerProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How is your mood Happy or Sad");
+            string message = Console.ReadLine();
+            MoodAnalyser moodAnalyzer = new MoodAnalyser();
+            Console.WriteLine(moodAnalyzer.AnalyseMood(message));
+        }
+    }
+}
